0d783d7a28d12333d5aae47e6e4efe628b9ff8a6
camilalocatelli <locatelli.camila6@gmail.com>
2022-03-19 03:33:56 -0300

    Finalizado!

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase, dislalia;
+
+Console.Write("Digite uma flase: ");
+frase = Console.ReadLine()!;
+
+dislalia = frase
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.WriteLine (dislalia);
